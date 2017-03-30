06b3b3493a67fc3050661db772528ed1de782ff5
Danny McMurrough <mcmurrough1dan@gmail.com>
2017-03-30 23:37:08 +0000

    Basic Template Added

--- nodeproj/app.js
+++ nodeproj/app.js
@@ -1,2 +1,17 @@
-var mochi = "frankenBitch"
-console.log(mochi);
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT; //Use for could9 autoassign port
+
+app.get('/', function(req, res){
+   res.send("Killin it."); 
+});
+
+app.get('/mochi', function(req, res){
+   res.send("Lovvin it."); 
+});
+
+app.listen(port, function(err){
+    console.log("The server is running on port: " + port);
+});
+

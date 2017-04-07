490d4c4d3fcf451e00546bc231a7b8a29396b8d1
Danny McMurrough <mcmurrough1dan@gmail.com>
2017-04-07 04:05:06 +0000

    comment out route

--- nodeproj/app.js
+++ nodeproj/app.js
@@ -12,7 +12,7 @@ app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-   //res.send(index.html); 
+   //res.send(index.html);
    res.render('index', { 
        list: ['frist val', '2nd val', '3rd val'], 
        nav: [
